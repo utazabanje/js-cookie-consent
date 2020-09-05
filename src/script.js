7240b9ab0a64b1bffe1466ad1d526ac18d336270
Miodrag Piletic <miodrag@expatrio.com>
2020-09-05 13:25:14 +0200

    add set cookie and fix styling

--- src/script.js
+++ src/script.js
@@ -1,36 +1,83 @@
-function cookieConsent(cookieName) {
-  let cookieAvailable = getCookie(cookieName)
-  let openSettings = document.getElementById('open-settings');
+const cookieConsent = (config) => {
+  sessionStorage.setItem('categories', JSON.stringify(['essential']))
+  let cookieAvailable = getCookie(config.name)
+  let openSettings = document.getElementById('openCookieSettings');
   let box = document.getElementById('js-cookie-consent-box');
+  let cookieToggleBox = document.querySelector('.js-cookie-consent-toogle-box');
   let switchBtn = document.querySelectorAll('.switch');
-  let categoriesGDPR = ['essential, marketing, analytics'];
-
-  console.log(cookieAvailable)
+  let saveCookiesSettings = document.getElementById('saveCookieSettings');
+  let acceptAllCookies = document.getElementById('acceptAllCookies');
+  
 
   if (cookieAvailable !== null) {
     box.style.display = 'none';
     return
   } else {
-    openSettings.addEventListener('click', function() {
-      var isOpen = box.classList.contains('slide-up');
+    cookieToggleBox.style.display = 'none';
+
+    openSettings.addEventListener('click', () => {
+      let isOpen = box.classList.contains('slide-up');
       box.setAttribute('class', isOpen ? 'slide-down' : 'slide-up');
+
+      if (cookieToggleBox.style.display === 'none') {
+        cookieToggleBox.style.display = 'block';
+      } else {
+        setTimeout(() => {
+          cookieToggleBox.style.display = 'none';
+        }, 750)
+      }
     });
   
-    switchBtn.forEach(function(item) {
-      item.addEventListener('click', function() {
-        if (this.className.includes('essential')) {
-          console.log(this)
-        } else if (this.className.includes('marketing')) {
-  
-        } else {
-  
+    switchBtn.forEach((item) => {
+      item.addEventListener('click', (e) => {
+        e.stopImmediatePropagation();
+        e.preventDefault();
+
+        if (e.target.className.includes('marketing')) {
+          toggleChecked(e.target);
+          toggleValueInArray('marketing');
+        } else if (e.target.className.includes('analytics')) {
+          toggleChecked(e.target);
+          toggleValueInArray('analytics');
         }
       })
-    })
-  }
-}
+    });
 
-function getCookie(name) {
+    saveCookiesSettings.addEventListener('click', () => {
+      let savedCookies = sessionStorage.getItem('categories');
+
+      setCookie('cookiesGDPR', savedCookies, 7)
+      box.style.display = 'none';
+    });
+
+    acceptAllCookies.addEventListener('click', () => {
+      setCookie('cookiesGDPR', JSON.stringify(['essential', 'marketing', 'analytics']), 7)
+      box.style.display = 'none';
+    });
+  }
+};
+
+const toggleValueInArray = (value) => {
+  let categories = sessionStorage.getItem('categories');
+  let categoriesGDPR = JSON.parse(categories);
+  let idx = categoriesGDPR.indexOf(value);
+
+  if (idx !== -1) {
+    categoriesGDPR.splice(idx, 1);
+  } else {
+    categoriesGDPR.push(value);
+  }
+
+  sessionStorage.setItem('categories', JSON.stringify(categoriesGDPR));
+};
+
+const toggleChecked = (el) => {
+  let checked = el.parentNode.childNodes[1].hasAttribute('checked');
+
+  return (!checked) ? el.parentNode.childNodes[1].setAttribute('checked', '') : el.parentNode.childNodes[1].removeAttribute('checked');
+};
+
+const getCookie = (name) => {
 	if (document.cookie.length) {
 		let nameEQ = name + '='
 		let ca = document.cookie.split(';')
@@ -39,16 +86,25 @@ function getCookie(name) {
 			let c = ca[i]
 
 			while (c.charAt(0) === ' ') {
-				c = c.substring(1, c.length)
+				c = c.substring(1, c.length);
 			}
 
 			if (c.indexOf(nameEQ) === 0) {
-				return c.substring(nameEQ.length, c.length)
+				return c.substring(nameEQ.length, c.length);
 			}
 		}
 	}
 
-	return null
-}
+	return null;
+};
 
-cookieConsent('cookiesGDPR')
+const setCookie = (name, value, exdays, path) => {
+	let d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	let expires = 'expires='+ d.toUTCString();
+
+	document.cookie = name + '=' + value + ';' + expires + ';path='+(path ? path : '/');
+};
+
+
+cookieConsent({name: 'cookiesGDPR'});
